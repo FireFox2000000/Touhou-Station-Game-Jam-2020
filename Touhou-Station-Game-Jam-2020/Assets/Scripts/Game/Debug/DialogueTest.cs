414350534eaa953b7e84014004daef7d46b68eb9
FireFox <FireFox2000000@gmail.com>
2020-08-20 15:02:51 +1000

    Imeplemented TMPro paging truncation into the dialog box system so that text is cut off properly. Created dialogue box prefab for future use.

--- Touhou-Station-Game-Jam-2020/Assets/Scripts/Game/Debug/DialogueTest.cs
+++ Touhou-Station-Game-Jam-2020/Assets/Scripts/Game/Debug/DialogueTest.cs
@@ -4,22 +4,12 @@ using UnityEngine;
 using UnityEngine.UI;
 using MoonscraperEngine;
 
-public class DialogueTest : MonoBehaviour, IDialogueUI
+public class DialogueTest : MonoBehaviour
 {
     [SerializeField]
-    Text text;
-    [SerializeField]
     TextAsset dialogueScript;
-
-    public bool AdvanceSequence()
-    {
-        return true;
-    }
-
-    public void SetCurrentSequence(DialogueScript.Sequence sequence)
-    {
-        text.text = sequence.text;
-    }
+    [SerializeField]
+    StandardDialogManager dialogueManager;
 
     DialogueScript m_testScript;
     StateMachine m_cutsceneStateMachine = new StateMachine();
@@ -29,25 +19,16 @@ public class DialogueTest : MonoBehaviour, IDialogueUI
     {
         m_testScript = JsonUtility.FromJson<DialogueScript>(dialogueScript.text);
 
-        m_cutsceneStateMachine.currentState = new DialogueRulestate(m_cutsceneStateMachine, m_testScript, this, () => { Close(); m_cutsceneStateMachine.currentState = null; });
-
-        //string jsonScript = JsonUtility.ToJson(m_testScript, true);
-        //System.IO.File.WriteAllText("test_dialogue_1.json", jsonScript);
+        m_cutsceneStateMachine.currentState = new DialogueRulestate(m_cutsceneStateMachine, m_testScript, dialogueManager, 
+            () => {
+                dialogueManager.Close();
+                m_cutsceneStateMachine.currentState = null;
+            });
     }
 
     // Update is called once per frame
     void Update()
     {
         m_cutsceneStateMachine.Update();
-    }
-
-    public void Open()
-    {
-        gameObject.SetActive(true);
-    }
-
-    public void Close()
-    {
-        gameObject.SetActive(false);
     }
 }
---
+++ Touhou-Station-Game-Jam-2020/Assets/Scripts/Game/UI/Dialog/StandardDialogManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Basic dialog display in pages with no text text animation
+/// </summary>
+[RequireComponent(typeof(TMPro.TextMeshProUGUI))]
+public class StandardDialogManager : MonoBehaviour, IDialogueUI
+{
+    TMPro.TextMeshProUGUI text;
+
+    string m_remainingSequenceText = string.Empty;
+
+    void Awake()
+    {
+        text = GetComponent<TMPro.TextMeshProUGUI>();
+    }
+
+    public bool AdvanceSequence()
+    {
+        Debug.Log(string.Format("Dialogue advance sequence. Current page ({0}/{1})", text.pageToDisplay, text.textInfo.pageCount));
+
+        if (text.pageToDisplay < text.textInfo.pageCount)
+        {
+            ++text.pageToDisplay;
+            return false;
+        }
+        else
+        {
+            text.pageToDisplay = 1;     // Reset back to the first page, otherwise we might be on an invalid page for the next sequence
+            return true;
+        }
+    }
+
+    public void SetCurrentSequence(DialogueScript.Sequence sequence)
+    {
+        text.text = sequence.text;
+    }
+
+    public void Open()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Close()
+    {
+        gameObject.SetActive(false);
+    }
+}
