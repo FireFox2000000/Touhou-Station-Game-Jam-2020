7c4c546c2fcb7380de4da2ae52c311e3465e82d9
FireFox <FireFox2000000@gmail.com>
2020-08-21 11:09:59 +1000

    Disable line renderer in game view during play mode, however keep it visiable in the scene view.

--- Touhou-Station-Game-Jam-2020/Assets/Scripts/Game/Movement/LineRendererPathMovement.cs
+++ Touhou-Station-Game-Jam-2020/Assets/Scripts/Game/Movement/LineRendererPathMovement.cs
@@ -18,11 +18,23 @@ public class LineRendererPathMovement : PathMovement
 
     private void Start()
     {
-        //m_lineRenderer.enabled = false; // Only need to run this on for debugging
+        m_lineRenderer.enabled = false; // Only need to run this on for debugging
     }
 
     public override Vector3[] GetPath()
     {
         return m_positions;
     }
+
+#if UNITY_EDITOR
+    private void Update()
+    {
+        // Draw the curve in the scene view
+        Vector3 position = transform.position;
+        for (int i = 1; i < m_positions.Length; ++i)
+        {
+            Debug.DrawLine(m_positions[i - 1] + position, m_positions[i] + position, Color.magenta);
+        }
+    }
+#endif
 }
