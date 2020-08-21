1291850c28a4b10217e69f00eb7660e8656a7d45
FireFox <FireFox2000000@gmail.com>
2020-08-21 10:43:10 +1000

    Implemented line renderer path-based movement system

--- Touhou-Station-Game-Jam-2020/Assets/Scripts/Game/PlayerScript.cs
+++ Touhou-Station-Game-Jam-2020/Assets/Scripts/Game/PlayerScript.cs
@@ -13,6 +13,10 @@ public class PlayerScript : MonoBehaviour
     public float HoriSpeed;
     public int SegmentDistance;
 
+    [SerializeField]
+    PathMovement m_movementPath;
+    Vector3[] m_path;
+    float m_currentPathDistance = 0;
 
     [System.Serializable]
     public class MovementStamps {
@@ -25,7 +29,7 @@ public class PlayerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        m_path = m_movementPath.GetPath();
     }
     public void IncrementHaniwa()
     {
@@ -46,15 +50,53 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        // Player Movement Control
-        PlayerTrans.position = new Vector3(PlayerTrans.position.x, PlayerTrans.position.y + (Input.GetAxis("Vertical") * Speed * Time.deltaTime), PlayerTrans.position.z);
-        if (PlayerTrans.position.y < -8.5) PlayerTrans.position = new Vector3(PlayerTrans.position.x, -8.5f, PlayerTrans.position.z);
-        if (PlayerTrans.position.y > -1.5) PlayerTrans.position = new Vector3(PlayerTrans.position.x, -1.5f, PlayerTrans.position.z);
-        if (Input.GetAxis("Horizontal") > 0)
-        {
+        
 
-            // if (StampStatus < Stamps[CurrentStamp].MovementLength && CurrentStamp== Stamps.Length-1)
-            // {
+        if (m_movementPath) // Fall back to the old pathing system
+        {
+            float movementInput = Input.GetAxis("Horizontal");
+            //int inputDirection = movementInput != 0 ? (int)Mathf.Sign(movementInput) : 0;
+            m_currentPathDistance += movementInput * HoriSpeed;
+
+            // Brute forced, can be optimised by preprocessing the path lengths but meh
+            float pathDistanceSqr = 0;
+            float desiredPathDistanceSqr = m_currentPathDistance;// * m_currentPathDistance;
+            Vector3 playerPosition = Vector3.zero;
+            Quaternion playerRotation = Quaternion.identity;
+
+            // Search to find where we should be along the path
+            for (int i = 1; i < m_path.Length; ++i)
+            {
+                float newPathDistanceSqr = pathDistanceSqr + (m_path[i] - m_path[i - 1]).magnitude;     // sqrMag cause much faster than regular mag, may as well optimise a little. 
+                if (newPathDistanceSqr > desiredPathDistanceSqr)
+                {
+                    float t = (desiredPathDistanceSqr - pathDistanceSqr) / (newPathDistanceSqr - pathDistanceSqr);
+                    playerPosition = Vector3.Lerp(m_path[i - 1], m_path[i], t);
+
+                    Vector3 direction = (m_path[i] - m_path[i - 1]).normalized;
+                    playerRotation = Quaternion.LookRotation(Quaternion.AngleAxis(-90, Vector3.up) * direction);    // Rotate the sprites so they actually face the camera
+
+                    break;
+                }
+
+                pathDistanceSqr = newPathDistanceSqr;
+            }
+
+            float yPos = transform.position.y + Input.GetAxis("Vertical") * Speed * Time.deltaTime;
+            transform.position = new Vector3(playerPosition.x, yPos, playerPosition.z);
+            transform.rotation = playerRotation;
+        }
+        else  // Older movement system
+        {
+            // Player Movement Control
+            PlayerTrans.position = new Vector3(PlayerTrans.position.x, PlayerTrans.position.y + (Input.GetAxis("Vertical") * Speed * Time.deltaTime), PlayerTrans.position.z);
+            if (PlayerTrans.position.y < -8.5) PlayerTrans.position = new Vector3(PlayerTrans.position.x, -8.5f, PlayerTrans.position.z);
+            if (PlayerTrans.position.y > -1.5) PlayerTrans.position = new Vector3(PlayerTrans.position.x, -1.5f, PlayerTrans.position.z);
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+
+                // if (StampStatus < Stamps[CurrentStamp].MovementLength && CurrentStamp== Stamps.Length-1)
+                // {
 
 
 
@@ -66,14 +108,14 @@ public class PlayerScript : MonoBehaviour
                     CurrentStamp += 1;
                     StampStatus = 0;
                 }
-            // }
-            
+                // }
 
-        }
-        if (Input.GetAxis("Horizontal") < 0)
-        {
-            // if (StampStatus > -1 && CurrentStamp == 0)
-            // {
+
+            }
+            if (Input.GetAxis("Horizontal") < 0)
+            {
+                // if (StampStatus > -1 && CurrentStamp == 0)
+                // {
                 StampStatus -= 1;
                 transform.Translate(Vector3.right * -HoriSpeed, Space.Self);
                 PlayerTrans.Rotate(0, -Stamps[CurrentStamp].RotationSpeed, 0, Space.World);
@@ -82,13 +124,10 @@ public class PlayerScript : MonoBehaviour
                     CurrentStamp -= 1;
                     StampStatus = Stamps[CurrentStamp].MovementLength;
                 }
-            // }
-            
+                // }
+
+            }
         }
-        
-
-
-
 
         // Saving previous player positions to create a path for the other segments to follow along
         bool SaveData = false;
