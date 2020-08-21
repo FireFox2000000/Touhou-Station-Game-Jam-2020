defd6356d3a2257dc9763b2cfca69c38c6d70fca
FireFox <FireFox2000000@gmail.com>
2020-08-21 15:33:10 +1000

    Turn off all frontend menus at scene load to speed up debugging on singles menus.

--- Touhou-Station-Game-Jam-2020/Assets/Scripts/Game/UI/Menus/FrontendMenuBase.cs
+++ Touhou-Station-Game-Jam-2020/Assets/Scripts/Game/UI/Menus/FrontendMenuBase.cs
@@ -6,16 +6,24 @@ using MoonscraperEngine;
 public abstract class FrontendMenuBase : MonoBehaviour, StateMachine.IState
 {
     StateMachine m_stateMachine;
+    bool m_enterAwake = false;
 
     public void Init(StateMachine sm)
     {
         m_stateMachine = sm;
     }
 
+    void Awake()
+    {
+        if (!m_enterAwake)
+            gameObject.SetActive(false);        // When first playing the scene, helps speed up debugging
+    }
+
     public void Enter()
     {
         Debug.Assert(m_stateMachine != null, "Must call Init() method before changing state");
 
+        m_enterAwake = true;
         gameObject.SetActive(true);
     }
 
