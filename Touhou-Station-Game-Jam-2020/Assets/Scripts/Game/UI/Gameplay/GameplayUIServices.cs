dcf50a077e349030e57e7f71c539288e9b92d601
FireFox <FireFox2000000@gmail.com>
2020-08-21 16:53:30 +1000

    Implemented smooth loading screen transition into gameplay.

---
+++ Touhou-Station-Game-Jam-2020/Assets/Scripts/Game/UI/Gameplay/GameplayUIServices.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayUIServices : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponentInChildren<UIFadeBlocker>(true).FadeOut(() => { });     // We assume we're coming from the frontend state
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
