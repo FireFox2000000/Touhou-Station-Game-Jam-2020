60efe2b5adf47a6bea7676b923bf3d36c9697564
FireFox <FireFox2000000@gmail.com>
2020-08-21 15:28:09 +1000

    Hooked up main gameplay scene to play button. Hooked up exit button. Credits screen skeleton.

--- Touhou-Station-Game-Jam-2020/Assets/Scripts/Game/UI/Menus/UIHomeMenu.cs
+++ Touhou-Station-Game-Jam-2020/Assets/Scripts/Game/UI/Menus/UIHomeMenu.cs
@@ -1,7 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIHomeMenu : FrontendMenuBase
 {
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
 }
