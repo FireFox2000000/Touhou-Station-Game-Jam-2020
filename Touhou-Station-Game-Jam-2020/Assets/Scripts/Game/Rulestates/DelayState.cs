c2edcd8dc0e1bc47e57c075648b3ca14fbedc810
FireFox <FireFox2000000@gmail.com>
2020-08-22 14:15:48 +1000

    Implemented epilogue cutscene.

---
+++ Touhou-Station-Game-Jam-2020/Assets/Scripts/Game/Rulestates/DelayState.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoonscraperEngine;
+
+public class DelayState : StateMachine.IState
+{
+    public delegate void OnCompleteFn();
+
+    float m_totalDelayTime;
+    float m_startTime;
+    OnCompleteFn m_onCompleteCallback;
+
+    public DelayState(float totalDelayTime, OnCompleteFn onCompleteCallback)
+    {
+        m_totalDelayTime = totalDelayTime;
+        m_onCompleteCallback = onCompleteCallback;
+    }
+
+    public void Enter()
+    {
+        m_startTime = Time.time;
+    }
+
+    public void Exit()
+    {
+    }
+
+    public void Update()
+    {
+        if (Time.time - m_startTime > m_totalDelayTime)
+        {
+            m_onCompleteCallback();
+        }
+    }
+}
---
+++ Touhou-Station-Game-Jam-2020/Assets/Scripts/Game/Rulestates/EpilogueSceneManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoonscraperEngine;
+
+public class EpilogueSceneManager : MonoBehaviour
+{
+    [SerializeField]
+    BackgroundBlending m_backgroundManager;
+    [SerializeField]
+    StandardDialogManager m_dialogueManager;
+    [SerializeField]
+    UIFadeBlocker m_loadingTransition;
+    [SerializeField]
+    string m_cutsceneKey;
+    [SerializeField]
+    Texture2D m_cutsceneStartTexture;
+    [SerializeField]
+    float m_dialogueStartDelay = 1.0f;
+    [SerializeField]
+    UIEndcardOptions m_endCardMenu;
+
+    StateMachine m_stateMachine = new StateMachine();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_endCardMenu.gameObject.SetActive(false);
+
+        Localiser.Instance.LocaliseSceneTextElements();
+
+        m_backgroundManager.SetTexture(m_cutsceneStartTexture);
+        m_loadingTransition.FadeOut(() => {
+            m_stateMachine.currentState = new DelayState(m_dialogueStartDelay, StartCutscene);
+        });
+    }
+
+    void Update()
+    {
+        m_stateMachine.Update();
+    }
+
+    void StartCutscene()
+    {
+        TextAsset introCutscene = Localiser.Instance.GetLocalisedDialogueFile(m_cutsceneKey);
+        DialogueScript script = JsonUtility.FromJson<DialogueScript>(introCutscene.text);
+        m_stateMachine.currentState = new DialogueRulestate(m_stateMachine, script, m_dialogueManager, OnCutsceneEnd);
+    }
+
+    void OnCutsceneEnd()
+    {
+        // Display thanks for playing image or something
+        m_stateMachine.currentState = new DelayState(m_dialogueStartDelay, () => {
+            m_endCardMenu.gameObject.SetActive(true);
+        });
+    }
+}
